d5907d84c9f063f7bcafaaa814e02b332a37e639
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2026-01-29 21:22:05 -0500

    it works and adds docker

--- main.py
+++ main.py
@@ -1,1 +1,80 @@
-curl https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor=densinium.bsky.social
+#curl https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor=densinium.bsky.social
+import requests
+import json
+import datetime
+import time
+import sys
+import os
+
+def load_user_data_from_file(did: str):
+    # load user data from the file, note that the folder may not exist
+    try:
+        with open(f"user_data/{did}/posts.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        pass
+
+    # if the file does not exist, return an empty list
+    return []
+
+def request_user_data(did: str, most_recent_timestamp: datetime.datetime):
+    # request user data from the API in 100 block messages, until the most recent cached post id is reached
+    all_posts = []
+    cursor = None
+    number_of_requests = 0
+    while True:
+        url = f"https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor={did}&limit=100"
+        if cursor:
+            url += f"&cursor={cursor}"
+        response = requests.get(url)
+        data = response.json()
+        posts = data["feed"]
+        for post in posts:
+            # if the post is older than the most recent cached post id, return all posts, timestamp uses ISO 8601 format
+            post_timestamp = datetime.datetime.fromisoformat(post["post"]["indexedAt"])
+            if most_recent_timestamp and post_timestamp <= most_recent_timestamp:
+                break
+            all_posts.append(post)
+        cursor = data.get("cursor")
+        if not cursor:
+            print("WARNING NO CURSOR")
+            break
+        number_of_requests += 1
+        time.sleep(1)  # to avoid rate limiting
+    print(f"Number of requests made: {number_of_requests}")
+    return all_posts
+
+
+def process_user(did: str):
+    data = load_user_data_from_file(did)
+    if data:
+        most_recent_cached_post_timestamp = datetime.datetime.fromisoformat(data[0]["post"]["indexedAt"])
+    else:
+        most_recent_cached_post_timestamp = None
+    new_posts = request_user_data(did, most_recent_cached_post_timestamp)
+    if new_posts:
+        print(f"Found {len(new_posts)} new posts for user {did}")
+        # prepend new posts to data
+        data = new_posts + data
+        # save data to file, creating the folder if it does not exist
+        os.makedirs(f"user_data/{did}", exist_ok=True)
+        with open(f"user_data/{did}/posts.json", "w") as file:
+            json.dump(data, file, indent=4)
+    else:
+        print(f"No new posts for user {did}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <username or did>")
+        sys.exit(1)
+    name = sys.argv[1]
+    #check if the username is a did, if not, convert it to a did
+    if not name.startswith("did:"):
+        # convert username to did
+        url = f"https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle={name}"
+        response = requests.get(url)
+        data = response.json()
+        did = data["did"]
+
+    process_user(did)
