843e9698cee421bc3291f523fe64dedc560967b1
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2026-01-31 17:13:58 -0500

    _more screaming_

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 #curl https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor=densinium.bsky.social
+print("AAAAAAAAAAA")
+print("importing os")
 import os
-from fetch_posts import process_user
-from util import ensure_did
+print("importing fetch_posts")
+import fetch_posts
+print("importing util")
+import util
+print("importing sys")
 import sys
+print("importing time")
+import time
 
 
 if __name__ == "__main__":
     #see if any users need to be added
+    # os.makedirs("user_data/testing", exist_ok=True)
     print("Hello world!")
+    time.sleep(1)
     try:
         with open("user_data/add_list.txt", "r") as file:
             for line in file:
                 name = line.strip()
                 if line.startswith("#") or not line:
                     continue
-                did = ensure_did(name)
+                did = util.ensure_did(name)
                 if did:
                     if os.path.exists(f"user_data/{did}"):
                         print("user already added, please remove them from add_list.txt")
@@ -27,7 +36,7 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 2:
         name = sys.argv[1]
-        did = ensure_did(name)
+        did = util.ensure_did(name)
         if did:
             #check if the did directory exists, if not create warn the user
             if os.path.exists(f"user_data/{did}"):
@@ -43,6 +52,6 @@ if __name__ == "__main__":
         if os.path.isdir(f"user_data/{did}"):
             try:
                 print(f"Processing user: {did}")
-                process_user(did)
+                fetch_posts.process_user(did)
             except Exception as e:
                 print(f"Error processing user {did}: {e}")
